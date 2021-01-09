994d06eb24307b86b171be28abb4503143e8baa6
ASAngelica <asa196549@gmail.com>
2021-01-09 16:35:02 -0300

    Referenciando os posts do usuário

--- src/main/java/com/lgdois/workshopmongo/domain/User.java
+++ src/main/java/com/lgdois/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.lgdois.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //Aqui no Spring para esta classe User corresponder a uma coleção lá do mongodb, que no nosso caso é a coleção "user",
@@ -19,6 +22,16 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	//Criar o atributo "posts", usando o annotation @DBRef, aqui no spring data para vc falar que um atributo esta referenciando
+	//outra coloeção do mongodb, basta você colocar o @DBRef e como boa prática incluir o atributo (lazy = true).
+	
+	//lazy = true -> como estamos referenciando uma coleção, por padrão, nós não queremos carregar automaticamente
+    //os posts quando  recuperar um usuário do banco de dados, ex: vc esta recuperando os usuários para fazer
+    //um relatório de usuários, a consulta retorna 1.000 usuários, se para cada usuário vier a lista de posts dele,
+    //será muito dado e tráfego na rede desnecessário.
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();// por boas práticas inciar a lista;
 	
 	public User() {
 	}
@@ -53,6 +66,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -78,4 +99,6 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 }
